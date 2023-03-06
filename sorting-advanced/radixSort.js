3e57959cf64da3c8228593e968550758c731c912
Olakunle Jamal <animashaunjamal700@gmail.com>
2023-03-06 22:08:43 +0100

    radix sort

---
+++ sorting-advanced/radixSort.js
@@ -0,0 +1,20 @@
+const getDigit = (num, i) => {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+};
+
+const digitCount = (num) => {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+};
+
+const mostDigits = (numbersArr) => {
+  let maxDigits = 0;
+  for (let i = 0; i < numbersArr.length; i++){
+    maxDigits = Math.max(maxDigits, digitCount(numbersArr[i]));
+  };
+
+  return maxDigits;
+
+};
+
+console.log(mostDigits([23, 567, 89, 12234324, 90]));
