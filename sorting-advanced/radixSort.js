184d0a93598ff8f4e0b15d43a503f0deeb066573
Olakunle Jamal <animashaunjamal700@gmail.com>
2023-03-07 18:17:47 +0100

    more radix sort

--- sorting-advanced/radixSort.js
+++ sorting-advanced/radixSort.js
@@ -18,3 +18,23 @@ const mostDigits = (numbersArr) => {
 };
 
 console.log(mostDigits([23, 567, 89, 12234324, 90]));
+
+const radixSort = (numbersArr) => {
+  let maxDigitCount = mostDigits(numbersArr);
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < numbersArr.length; i++) {
+      let digit = getDigit(numbersArr[i], k);
+      digitBuckets[digit].push(numbersArr[i]);
+    };
+
+    // console.log(digitBuckets);
+    numbersArr = [].concat(...digitBuckets);
+    // console.log(numbersArr);
+  };
+
+  return numbersArr;
+
+}
+
+console.log(radixSort([23, 345, 5467, 12, 2345, 9852]));
