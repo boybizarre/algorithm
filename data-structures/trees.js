2fc9aa6f78ddac1696746f550c64c3ce611d9b35
Olakunle Jamal <animashaunjamal700@gmail.com>
2023-03-18 22:02:09 +0100

    binary search trees

---
+++ data-structures/trees.js
@@ -0,0 +1,113 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        // without the next two lines of code there would be an infinite loop
+        if (!value) return undefined;
+        if (value === current.value) return undefined;
+
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  find(value) {
+    if (!value) return undefined;
+    if (this.root === null) return false;
+
+    let current = this.root,
+      found = false;
+    
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+
+    if (!found) return undefined;
+    return current;
+  }
+
+  contains(value) {
+    if (!value) return false;
+    if (this.root === null) return false;
+
+    let current = this.root,
+      found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
+
+var tree = new BinarySearchTree();
+
+tree.insert(10);
+tree.insert(5);
+tree.insert(2);
+tree.insert(7);
+tree.insert(13);
+tree.insert(11);
+tree.insert(16);
+
+
+console.log(tree.find(5))
+console.log(tree.contains())
+console.log(tree);
+
+// tree.root = new Node(10);
+// tree.root.left = new Node(7);
+// tree.root.right = new Node(15);
+// tree.root.left.right = new Node(9);
+
+
+// BIG O notation of BST
+
+// Insertion - O(log n)
+// Searching - O(log n)
+
+// NOT guaranteed!
